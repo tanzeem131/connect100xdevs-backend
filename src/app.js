4f8eb6945e6c978dbc2b5af4c1b41c5b61a15c83
tanzeem131 <dev.tanzeem@gmail.com>
2024-09-29 00:18:45 +0530

    Handling multiple routers

--- src/app.js
+++ src/app.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Tanzeem", Age: "22" });
-});
-
-app.post("/user", (req, res) => {
-  res.send("Data successfully saved in database");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Data is deleted successfully");
-});
-
-//"use" will call for every method(get,post,put etc)
-app.use("/test", (req, res) => {
-  res.send("Hello from server:Testpage");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("handling the route 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route 2");
+    // res.send("Response from Route handler 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route 3");
+    // res.send("Response from Route handler 3");
+    next();
+  },
+  (req, res) => {
+    console.log("handling the route 4");
+    res.send("Response from Route handler 4");
+  }
+);
 
 app.listen(4000, () => {
   console.log("server is running on port:4000");
