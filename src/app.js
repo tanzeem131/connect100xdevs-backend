276c03d565bf8c5a90633f2580e143996b851880
tanzeem131 <dev.tanzeem@gmail.com>
2024-09-28 20:05:43 +0530

    exploring get, post, put methods

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
-app.use("/", (req, res) => {
-  res.send("Hello from server:Dashboard");
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Tanzeem", Age: "22" });
 });
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved in database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data is deleted successfully");
+});
+
+//"use" will call for every method(get,post,put etc)
 app.use("/test", (req, res) => {
   res.send("Hello from server:Testpage");
 });
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello Hello:HelloPage");
-});
+
 app.listen(4000, () => {
   console.log("server is running on port:4000");
 });
