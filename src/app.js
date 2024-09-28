9d5c50ec4f70879dcc105f6d922ea04d532af6e9
tanzeem131 <dev.tanzeem@gmail.com>
2024-09-29 01:31:09 +0530

    Error handlin using try and catch and app.use

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const app = express();
 
-const { AdminAuth, UserAuth } = require("./middlewares/AdminAuth");
+app.get("/user", (req, res) => {
+  //1st way- error handling try catch method
+  try {
+    throw new Error("ajnsjjdnjd");
+    res.send("User data sent");
+  } catch (err) {
+    res.status(500).send("Something went wrong!!!");
+  }
+});
 
-app.use("/admin", AdminAuth);
-
-app.get("/user", UserAuth, (req, res) => {
-  res.send("User data sent");
-});
-app.get("/admin/getAllData", (req, res) => {
-  //Logic to check user is authorized or not
-  res.send("All data sent");
-});
-app.get("/admin/deleteUser", (req, res) => {
-  //Logic to check user is authorized or not
-  res.send("Deleted a User");
-});
+//2nd way -error handling
+// app.use("/", (err, req, res, next) => {
+//   if (err) {
+//     res.status(500).send("Something went wrong!!!111");
+//   }
+// });
 
 app.listen(4000, () => {
   console.log("server is running on port:4000");
