208aaa8a6869a0c6949607fc0cc398c3a2f50236
Tanzeem <dev.tanzeem@gmail.com>
2025-07-21 02:49:05 +0530

    socket event

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,12 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId, targetUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("$"))
+    .digest("hex");
+};
 
 const initializeSocket = (server) => {
   const io = socket(server, {
@@ -7,11 +15,18 @@ const initializeSocket = (server) => {
 
   io.on("connection", (socket) => {
     //handle events
-    socket.on("joinChat", {});
+    socket.on("joinChat", ({ userId, targetUserId }) => {
+      const roomId = getSecretRoomId(userId, targetUserId);
+      console.log("joining room with id: ", roomId);
+      socket.join(roomId);
+    });
 
-    socket.on("sendMessage", {});
+    socket.on("sendMessage", ({ userId, targetUserId, newMessage }) => {
+      const roomId = getSecretRoomId(userId, targetUserId);
+      io.to(roomId).emit("messageReceived", { newMessage });
+    });
 
-    socket.on("disconnect", {});
+    socket.on("disconnect", () => {});
   });
 };
 
