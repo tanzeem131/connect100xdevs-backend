8c7fc25b9aac7e19940b4bc446bdd79bd60c2c4d
Tanzeem <dev.tanzeem@gmail.com>
2025-07-27 23:58:07 +0530

    Integrated github OAuth authentication

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,116 @@
+// const mongoose = require("mongoose");
+// const validator = require("validator");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     firstName: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//       minlength: 2,
+//       maxlength: 30,
+//     },
+//     lastName: {
+//       type: String,
+//       trim: true,
+//       minlength: 2,
+//       maxlength: 30,
+//     },
+//     emailId: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       lowercase: true,
+//       trim: true,
+//       maxlength: 40,
+//       validate(value) {
+//         if (!validator.isEmail(value)) {
+//           throw new Error("Email id is invalid");
+//         }
+//       },
+//     },
+//     githubUsername: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       trim: true,
+//       maxlength: 30,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+//     age: {
+//       type: Number,
+//       min: 18,
+//       max: 80,
+//     },
+//     gender: {
+//       type: String,
+//       trim: true,
+//       enum: {
+//         values: ["male", "female", "other"],
+//         message: `{VALUE} is not a valid gender type`,
+//       },
+//     },
+//     photoUrl: {
+//       type: String,
+//       default: "https://geographyandyou.com/images/user-profile.png",
+//       validate(value) {
+//         if (!validator.isURL(value)) {
+//           throw new Error("Invalid Photo URL: " + value);
+//         }
+//       },
+//       maxlength: 500,
+//     },
+//     about: {
+//       type: String,
+//       default: "Even I don't know about me",
+//       maxlength: 200,
+//     },
+//     skills: {
+//       type: [String],
+//       validate: {
+//         validator: function (skills) {
+//           return (
+//             skills.length <= 5 && skills.every((skill) => skill.length <= 50)
+//           );
+//         },
+//         message:
+//           "You can only specify up to 5 skills, each up to 50 characters long.",
+//       },
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// userSchema.methods.getJWT = async function () {
+//   const user = this;
+//   const token = await jwt.sign({ _id: user._id }, process.env.SECRET_KEY, {
+//     expiresIn: "7d",
+//   });
+//   return token;
+// };
+
+// userSchema.methods.validatePassword = async function (passwordInputByUser) {
+//   const user = this;
+//   const passwordHash = user.password;
+//   const isvalidatePassword = await bcrypt.compare(
+//     passwordInputByUser,
+//     passwordHash
+//   );
+//   return isvalidatePassword;
+// };
+
+// module.exports = mongoose.model("User", userSchema);
+
 const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -15,7 +124,6 @@ const userSchema = new mongoose.Schema(
     lastName: {
       type: String,
       trim: true,
-      minlength: 2,
       maxlength: 30,
     },
     emailId: {
@@ -31,16 +139,17 @@ const userSchema = new mongoose.Schema(
         }
       },
     },
+    githubId: {
+      type: String,
+      unique: true,
+      required: true,
+    },
     githubUsername: {
       type: String,
       required: true,
       unique: true,
       trim: true,
-      maxlength: 30,
-    },
-    password: {
-      type: String,
-      required: true,
+      maxlength: 100,
     },
     age: {
       type: Number,
@@ -67,8 +176,8 @@ const userSchema = new mongoose.Schema(
     },
     about: {
       type: String,
-      default: "Even I don't know about me",
-      maxlength: 200,
+      default: "Even I don't know about myself much",
+      maxlength: 300,
     },
     skills: {
       type: [String],
@@ -82,6 +191,10 @@ const userSchema = new mongoose.Schema(
           "You can only specify up to 5 skills, each up to 50 characters long.",
       },
     },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
@@ -90,20 +203,19 @@ const userSchema = new mongoose.Schema(
 
 userSchema.methods.getJWT = async function () {
   const user = this;
-  const token = await jwt.sign({ _id: user._id }, process.env.SECRET_KEY, {
+  const payload = {
+    _id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    emailId: user.emailId,
+    photoUrl: user.photoUrl,
+    about: user.about,
+    githubUsername: user.githubUsername,
+  };
+  const token = await jwt.sign(payload, process.env.SECRET_KEY, {
     expiresIn: "7d",
   });
   return token;
-};
-
-userSchema.methods.validatePassword = async function (passwordInputByUser) {
-  const user = this;
-  const passwordHash = user.password;
-  const isvalidatePassword = await bcrypt.compare(
-    passwordInputByUser,
-    passwordHash
-  );
-  return isvalidatePassword;
 };
 
 module.exports = mongoose.model("User", userSchema);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,3 +1,4 @@
+/*
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const express = require("express");
@@ -80,6 +81,162 @@ authRouter.post("/login", async (req, res) => {
     }
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+authRouter.post("/logout", async (req, res) => {
+  res
+    .cookie("token", null, { expires: new Date(Date.now()) })
+    .send("Logout successfully");
+});
+
+module.exports = authRouter;
+*/
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+//Login with github
+const express = require("express");
+const authRouter = express.Router();
+const User = require("../models/user");
+const axios = require("axios");
+
+authRouter.get("/github", (req, res) => {
+  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=user:email`;
+  res.redirect(githubAuthUrl);
+});
+
+authRouter.get("/github/callback", async (req, res) => {
+  const { code } = req.query;
+
+  if (!code) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Authorization code not provided." });
+  }
+
+  try {
+    // 1. Exchange the code for an access token
+    const tokenResponse = await axios.post(
+      "https://github.com/login/oauth/access_token",
+      {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code: code,
+      },
+      {
+        headers: { Accept: "application/json" },
+      }
+    );
+
+    const { access_token } = tokenResponse.data;
+
+    if (!access_token) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Failed to retrieve access token." });
+    }
+
+    // 2. Use the access token to get user's profile info
+    const userResponse = await axios.get("https://api.github.com/user", {
+      headers: { Authorization: `token ${access_token}` },
+    });
+
+    const {
+      id: githubId,
+      name,
+      email: githubEmail,
+      avatar_url,
+      login: githubUsername,
+    } = userResponse.data;
+
+    // 3. Get user's primary email if not public
+    let primaryEmail = githubEmail;
+    if (!primaryEmail) {
+      const emailsResponse = await axios.get(
+        "https://api.github.com/user/emails",
+        {
+          headers: { Authorization: `token ${access_token}` },
+        }
+      );
+      const primary = emailsResponse.data.find((e) => e.primary && e.verified);
+      primaryEmail = primary ? primary.email : null;
+    }
+
+    if (!primaryEmail) {
+      return res.status(400).json({
+        success: false,
+        message: "Could not retrieve a verified primary email from GitHub.",
+      });
+    }
+
+    // 4. Find or create a user in your database
+    let user = await User.findOne({ emailId: primaryEmail });
+
+    if (!user) {
+      user = await User.findOne({ githubId: githubId });
+    }
+
+    const nameParts = name ? name.split(" ") : [];
+    const firstName = nameParts[0] || githubUsername;
+    const lastName = nameParts.slice(1).join(" ") || "";
+
+    if (user) {
+      user.githubId = user.githubId || githubId;
+      user.githubUsername = user.githubUsername || githubUsername;
+      user.photoUrl =
+        user.photoUrl === "https://geographyandyou.com/images/user-profile.png"
+          ? avatar_url
+          : user.photoUrl;
+      user.isVerified = true;
+
+      await user.save();
+    } else {
+      user = new User({
+        firstName: firstName,
+        lastName: lastName,
+        emailId: primaryEmail,
+        githubId: githubId,
+        githubUsername: githubUsername,
+        photoUrl: avatar_url,
+        isVerified: true,
+      });
+      await user.save();
+    }
+
+    // 5. Generate JWT token
+    const token = await user.getJWT();
+
+    // 6. Set token in cookie
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    // 7. Redirect to frontend
+    res.redirect("http://localhost:5173");
+  } catch (error) {
+    console.error(
+      "GitHub auth error:",
+      error.response ? error.response.data : error.message
+    );
+    res.status(500).json({
+      success: false,
+      message: "Internal server error during GitHub authentication.",
+    });
   }
 });
 
